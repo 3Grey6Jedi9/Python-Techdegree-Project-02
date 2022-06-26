7c68e53c87196b2ac7f282bc95996b04de6f0d94
Darth Vader <danieltarancon@gmail.com>
2022-06-27 01:23:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,21 @@ while ValueError:
             choice = int(input("Enter an option --> "))
             if choice not in range(1,int(i)):
                 raise ValueError
-            print(functions.depure(constants.teams[choice-1], Teams[choice-1]))
+            depure = functions.depure(constants.teams[choice-1], Teams[choice-1])
+            players_on_Team = functions.players_on_Team(Teams[choice-1])
+            guardians_on_Team = functions.guardians_on_Team(Teams[choice-1])
+            print("""Team: {}
+---------------------------------------------------""".format(depure['Team']))
+            print("Total players: {}".format(depure['Total players']))
+            print("Total experienced: {}".format(depure['Total experienced']))
+            print("Total inexperienced: {}".format(depure['Total inexperienced']))
+            print("Average height: {}\n\n".format(depure['Average height']))
+            print("""Players on Team:
+    {}\n\n""".format(players_on_Team))
+            print("""Guardians:
+    {}\n\n""".format(guardians_on_Team))
+
+
         except ValueError:
             print("You must select one of the options using an integer from 1 to {}".format(i))
 
