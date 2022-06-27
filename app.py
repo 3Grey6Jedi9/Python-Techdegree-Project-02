ac72f54eae0ef757b545f5701520239d4e6731d0
Darth Vader <danieltarancon@gmail.com>
2022-06-27 02:42:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ Here are your choices:
     2) Quit\n""")
 
     try:
-        choice = int(input("Enter an option --> "))
+        choice = int(input("""Enter an option
+---->  """))
         if choice not in range(1,3):
             raise ValueError
 
@@ -29,27 +30,29 @@ Here are your choices:
                 i += 1
         else:
             exit()
-        try:
-            choice = int(input("Enter an option --> "))
-            if choice not in range(1,int(i)):
-                raise ValueError
-            depure = functions.depure(constants.teams[choice-1], Teams[choice-1])
-            players_on_Team = functions.players_on_Team(Teams[choice-1])
-            guardians_on_Team = functions.guardians_on_Team(Teams[choice-1])
-            print("""\nTeam: {}
+        while ValueError:
+            try:
+                choice = int(input("""Enter an option
+----> """))
+                if choice not in range(1,int(i)):
+                    raise ValueError
+                depure = functions.depure(constants.teams[choice-1], Teams[choice-1])
+                players_on_Team = functions.players_on_Team(Teams[choice-1])
+                guardians_on_Team = functions.guardians_on_Team(Teams[choice-1])
+                print("""\nTeam: {}
 ---------------------------------------------------""".format(depure['Team']))
-            print("Total players: {}".format(depure['Total players']))
-            print("Total experienced: {}".format(depure['Total experienced']))
-            print("Total inexperienced: {}".format(depure['Total inexperienced']))
-            print("Average height: {}\n\n".format(depure['Average height']))
-            print("""Players on Team:
+                print("Total players: {}".format(depure['Total players']))
+                print("Total experienced: {}".format(depure['Total experienced']))
+                print("Total inexperienced: {}".format(depure['Total inexperienced']))
+                print("Average height: {}\n\n".format(depure['Average height']))
+                print("""Players on Team:
     {}\n\n""".format(players_on_Team))
-            print("""Guardians:
+                print("""Guardians:
     {}\n\n""".format(guardians_on_Team))
-            key = input("Please press 'Enter' to continue...\n")
-
-        except ValueError:
-            print("You must select one of the options using an integer from 1 to {}".format(i))
+                key = input("Please press 'Enter' to continue...\n")
+                break
+            except ValueError:
+                print("You must select one of the options using an integer from 1 to {}\n".format(i-1))
 
     except ValueError:
-        print("You must enter 1 or 2 please")
+        print("You must enter 1 or 2 please\n")
